068d1ec999a154bf87b6ad5651507b8a589973bf
Priyabrata <priyabratachattaraj23@gmail.com>
2024-06-29 19:46:59 +0530

    cors fix

--- middlewares/cors.middleware.cjs
+++ middlewares/cors.middleware.cjs
@@ -1,24 +1,30 @@
-const { ALLOWED_ORIGINS } = require('../config.cjs')
+const { ALLOWED_ORIGINS, PROD } = require('../config.cjs')
 const cors = require('cors')
 const { log } = require('../utils')
 
 const corsOptions = {
     origin: (req_origin, callback) => {
-        if (!req_origin) return callback(null, true) // Allow non-req_origin requests (e.g., from Postman)
+        try {
+            if (!req_origin) {
+                if (PROD) throw new Error('Origin not provided')
+                return callback(null, true)
+            }
 
-        const req_host = new URL(req_origin).hostname
-        const isAllowed = ALLOWED_ORIGINS.some(
-            allowed_origin =>
-                (req_host === allowed_origin) ||
-                req_host.endsWith(`.${allowed_origin}`) ||
-                (req_host === new URL(allowed_origin).hostname)
-        )
+            const req_host = new URL(req_origin).hostname
+            const isAllowed = ALLOWED_ORIGINS.some(
+                allowed_origin =>
+                    (req_host === allowed_origin) ||
+                    req_host.endsWith(`.${allowed_origin}`) ||
+                    (req_host === new URL(allowed_origin).hostname)
+            )
+            if (!isAllowed) throw new Error(`Origin ${req_origin} not allowed`)
 
-        if (isAllowed) callback(null, true)
-        else {
-            log.error(`Cors.middleware : origin : ${req_origin} not allowed`)
+            return callback(null, true)
+        }
+        catch (err) {
+            log.error(`CORS.middleware : origin : error :${err.message}`)
 
-            callback(new Error('Not allowed by CORS'))
+            return callback(err, false)
         }
     },
     credentials: true
